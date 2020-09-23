1364417870a6057860af4768a069eb4ddce27d11
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-09-23 13:19:51 +0200

    Added BilTest to examples
    Code from sept 23 - Classes

---
+++ Bil.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilTest
+{
+    class Bil
+    {
+        //Medlemsvariabel/Inastansvariabel
+        public string Färg;
+        public string RegNummer;
+        private int MaxHastighet;
+        private int CurrentSpeed;
+        
+
+        public Bil(string Färg, string RegNummer, int MaxHastighet)
+        {
+            this.Färg = Färg;
+            this.RegNummer = RegNummer;
+            this.MaxHastighet = MaxHastighet;
+            CurrentSpeed = 0;
+        }
+
+        public Bil(){}
+
+        public string GetFärg()
+        {
+            return Färg;
+        }
+
+        public int GetMaxHastighet()
+        {
+            return MaxHastighet;
+        }
+
+        public void SetFärg(string färg)
+        {
+            if (färg == "Röd")
+            {
+                Färg = "Svart";
+            }
+            else
+            {
+                Färg = färg;
+            }
+        }
+
+        public int Gasa(int speed)
+        {
+            if (speed > MaxHastighet)
+            {
+                CurrentSpeed = MaxHastighet;
+            }
+            else
+            {
+                CurrentSpeed = speed;
+            }
+
+            return CurrentSpeed;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapar instans av Bil mha konstruktorn
+           // Bil bil = new Bil();
+
+            //Bil enAnnanBil = new Bil("Rosa", "AAA 111");
+
+           Bil bil = new Bil("Svart", "AAA 111", 180);
+
+            string färg = bil.GetFärg();
+            Console.WriteLine(färg);
+
+            bil.SetFärg("Röd");
+            bil.Färg = "Röd";
+            
+
+            Console.WriteLine(bil.GetFärg());
+            int hastighet = 200;
+
+            if (hastighet > bil.GetMaxHastighet())
+            {
+                Console.WriteLine($"Högsta tillåtna hastighet är {bil.GetMaxHastighet()}!");
+            }
+            else
+            {
+                Console.WriteLine($"Jag jör just nu i {bil.Gasa(hastighet)}");
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
